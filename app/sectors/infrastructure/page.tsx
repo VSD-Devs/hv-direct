81a76e6ac8c87e777dbe58f5b2e9114d9fb5afe0
Khalil2155 <khalil.kirkwood@hotmail.co.uk>
2025-05-07 23:07:28 +0100

    fixes 2

--- app/sectors/infrastructure/page.tsx
+++ app/sectors/infrastructure/page.tsx
@@ -1,14 +1,44 @@
-  {/* Hero Section */}
-  <section className="relative py-14 md:py-20 bg-cover bg-center text-white" style={{ 
-  {/* Introduction Section */}
-  <section className="py-14 bg-white">
-  {/* Services Section */}
-  <section id="services" className="py-14 bg-[#f5f5f5]">
-  {/* Process Section */}
-  <section className="py-14 bg-white">
-  {/* Projects Section */}
-  <section className="py-14 bg-[#f5f5f5]">
-  {/* Benefits Section */}
-  <section className="py-14 bg-white">
-  {/* Call to Action */}
-  <section className="py-14 bg-gradient-to-r from-[#0f5f96] to-[#2199ea] text-white"> 
+'use client';
+
+import React from 'react';
+
+export default function InfrastructurePage() {
+  return (
+    <main>
+      {/* Hero Section */}
+      <section className="relative py-14 md:py-20 bg-cover bg-center text-white">
+        {/* Hero content will go here */}
+      </section>
+
+      {/* Introduction Section */}
+      <section className="py-14 bg-white">
+        {/* Introduction content will go here */}
+      </section>
+
+      {/* Services Section */}
+      <section id="services" className="py-14 bg-[#f5f5f5]">
+        {/* Services content will go here */}
+      </section>
+
+      {/* Process Section */}
+      <section className="py-14 bg-white">
+        {/* Process content will go here */}
+      </section>
+
+      {/* Projects Section */}
+      <section className="py-14 bg-[#f5f5f5]">
+        {/* Projects content will go here */}
+      </section>
+
+      {/* Benefits Section */}
+      <section className="py-14 bg-white">
+        {/* Benefits content will go here */}
+      </section>
+
+      {/* Call to Action */}
+      <section className="py-14 bg-gradient-to-r from-[#0f5f96] to-[#2199ea] text-white">
+        {/* CTA content will go here */}
+      </section>
+    </main>
+  );
+} 
